89de011447b2ceaa9111b70e74f25b4cb3c393f7
Artin <lengthmin@gmail.com>
2021-03-16 01:41:08 +0800

    Separate code

---
+++ run.sh
@@ -0,0 +1,11 @@
+sudo qemu-system-x86_64 \
+  -m 4G \
+  -usb \
+  -smp 4 \
+  -drive file=./ubuntu-server1.qcow2,if=virtio \
+  -machine type=q35,accel=hvf \
+  -cpu host \
+  -device usb-tablet \
+  -nic user,model=virtio \
+  -netdev tap,id=secnet0,script=./tap-up.sh,downscript=./tap-down.sh \
+  -device e1000,netdev=secnet0,mac=52:55:00:d1:55:01
